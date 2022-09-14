099dd725148d18d62ff6d40eb75898dde04e9b62
JosephThuku <jthuku490@gmail.com>
2022-09-14 18:28:10 +0300

    second push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+/**
+* print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char i = 'a';
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
