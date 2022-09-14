2cf1cc4fe05e617933df455f0cee7e0a5150c521
JosephThuku <93326983+JosephThuku@users.noreply.github.com>
2022-09-14 21:15:04 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-void _putchar(char c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
+int _abs(int x);
 int print_last_digit(int);
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+int print_last_digit(int);
 void print_to_98(int n);
 void print_times_table(int n);
-void jack_bauer(void);
-#endif /* MAIN_H */
+void output_hundred();
+void output_unit();
+void output_tens();  
